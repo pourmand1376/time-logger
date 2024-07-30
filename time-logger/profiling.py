b7c47ba9df964c3c2b9d65757e537385da2fcc95
Amir Pourmand <pourmand1376@gmail.com>
2024-07-30 21:13:16 +0330

    update profiling logic

--- time-logger/profiling.py
+++ time-logger/profiling.py
@@ -1,43 +1,63 @@
 import inspect
-from logging import Logger
-
 import time
 from contextlib import suppress
 from functools import wraps
 from inspect import iscoroutinefunction
+from logging import Logger
+from typing import Callable
 
-class LoggerProfile():
-    def __init__(self, logger: Logger, log_start:bool,log_args:bool,log_kwargs:str) -> None:
-        pass
 
-    def get_class(self, method):
-        if inspect.ismethod(method):
-            return method.__self__.__class__
-        return None
+def get_class(method):
+    if inspect.ismethod(method):
+        return method.__self__.__class__
+    return None
 
-    def log_arguments(self, log_args,log_kwargs, *args, **kwargs):
+
+class Profiler:
+    def __init__(
+        self,
+        function: Callable,
+        args,
+        kwargs,
+        logger: Logger,
+        log_start: bool,
+        log_args: bool,
+        log_kwargs: str,
+    ) -> None:
+        self.main_function = function
+        self.args = args
+        self.kwargs = kwargs
+
+        self.logger = logger
+        self.log_start = log_start
+        self.log_args = log_args
+        self.log_kwargs = log_kwargs
+
+    def log_arguments(self):
         print_args = ""
         print_kwargs = ""
 
         with suppress(Exception):
-            if log_args:
-                print_args = f"print_args={str(args)}"
+            if self.log_args:
+                print_args = f"print_args={str(self.args)}"
 
-            if log_kwargs == "all":
-                print_kwargs = f"print_kwargs={str(kwargs)}"
-            elif log_kwargs:  # Changed condition to check if log_kwargs is not None
-                print_kwargs = f"print_kwargs={log_kwargs}:{kwargs.get(log_kwargs, None)}"
+            if self.log_kwargs == "all":
+                print_kwargs = f"print_kwargs={str(self.kwargs)}"
+            elif (
+                self.log_kwargs
+            ):  # Changed condition to check if log_kwargs is not None
+                print_kwargs = f"print_kwargs={self.log_kwargs}:{self.kwargs.get(self.log_kwargs, None)}"
 
         return print_args, print_kwargs
 
-    def start_log_logic(self, func,logger, log_start,log_args,log_kwargs, *args, **kwargs):
-        print_args, print_kwargs = self.log_arguments(log_args,log_kwargs,*args, **kwargs)
-        if log_start and logger:
-            cls = get_class(func)
+    def start_log_logic(self):
+        print_args, print_kwargs = self.log_arguments()
+        if self.log_start and self.logger:
+            cls = get_class(self.main_function)
             value_to_log = (
-                f"Starting {cls.__name__}.{func.__name__}()"
+                f"Starting {cls.__name__}.{self.main_function.__name__}()"
                 if cls
-                else f"Started {func.__name__}()"
+                else f"Started {self.main_function.__name__}()"
             )
 
             if print_args:
@@ -48,19 +68,21 @@ class LoggerProfile():
 
             value_to_log += "\n"
             print(value_to_log)
-            logger.info(value_to_log)
+            self.logger.info(value_to_log)
 
-    def end_log_logic(self, func,logger, log_start,log_args,log_kwargs, start_time, *args, **kwargs):
+    def end_log_logic(
+        self,
+    ):
         end_time = time.perf_counter()
-        run_time = end_time - start_time
+        run_time = end_time - self.start_time
 
-        print_args, print_kwargs = self.log_arguments(log_args,log_kwargs, *args, **kwargs)
-        if logger:
-            cls = self.get_class(func)
+        print_args, print_kwargs = self.log_arguments()
+        if self.logger:
+            cls = get_class(self.main_function)
             value_to_log = (
-                f"Finished {cls.__name__}.{func.__name__}() in {run_time:.4f} secs"
+                f"Finished {cls.__name__}.{self.main_function.__name__}() in {run_time:.4f} secs"
                 if cls
-                else f"Finished {func.__name__}() in {run_time:.4f} secs"
+                else f"Finished {self.main_function.__name__}() in {run_time:.4f} secs"
             )
 
             if print_args:
@@ -71,35 +93,58 @@ class LoggerProfile():
 
             value_to_log += "\n"
             print(value_to_log)
-            logger.info(value_to_log)
+            self.logger.info(value_to_log)
 
 
-def profiling(logger: Logger,
-              log_start: bool=False,
-               log_args: bool = False,
-                log_kwargs: str = None,
-                ):
+def profiling(
+    logger: Logger,
+    log_start: bool = False,
+    log_args: bool = False,
+    log_kwargs: str = None,
+):
     """
     We will write all the result into logger
     log_args can be true or false. If it is true, it will print all the args passed to the function in the log.
     log_kwargs can be None, "all" or argument_name to be logged.
     """
+
     def decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
-            start_log_logic(f,  *args, **kwargs)
+            profiler = Profiler(
+                f,
+                args,
+                kwargs,
+                logger=logger,
+                log_start=log_start,
+                log_args=log_args,
+                log_kwargs=log_kwargs,
+            )
+            profiler.start_log_logic()
             start_time = time.perf_counter()
+            profiler.start_time = start_time
             output_value = f(*args, **kwargs)
-            end_log_logic(f, logger, log_start, log_args, log_kwargs, start_time, *args, **kwargs)
+            profiler.end_log_logic()
             return output_value
 
         @wraps(f)
         async def async_wrapper(*args, **kwargs):
-            start_log_logic(f, logger, log_start,log_args,log_kwargs, *args, **kwargs)
+            profiler = Profiler(
+                f,
+                args,
+                kwargs,
+                logger=logger,
+                log_start=log_start,
+                log_args=log_args,
+                log_kwargs=log_kwargs,
+            )
+            profiler.start_log_logic()
             start_time = time.perf_counter()
+            profiler.start_time = start_time
             output_value = await f(*args, **kwargs)
-            end_log_logic(f, logger, log_start, log_args, log_kwargs, start_time, *args, **kwargs)
+            profiler.end_log_logic()
             return output_value
 
         return async_wrapper if iscoroutinefunction(f) else wrapper
+
     return decorator
