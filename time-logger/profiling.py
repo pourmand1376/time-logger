eb128952892975f2cf96370e65e0b0aabb8826dc
Amir Pourmand <pourmand1376@gmail.com>
2024-08-01 21:33:18 +0330

    add default logger and test

--- time-logger/profiling.py
+++ time-logger/profiling.py
@@ -1,16 +1,8 @@
-import inspect
 import time
-from contextlib import suppress
 from functools import wraps
 from inspect import iscoroutinefunction
 from logging import Logger
-from typing import Callable, Optional
-
-
-def get_class(method):
-    if inspect.ismethod(method):
-        return method.__self__.__class__
-    return None
+from typing import Callable, Optional, Union
 
 
 class Profiler:
@@ -32,29 +24,59 @@ class Profiler:
         self.log_args = log_args
         self.log_kwargs = log_kwargs
         self.start_time = 0.0
+        self.module_name = self._get_module_name()
 
     def _format_arguments(self) -> tuple[str, str]:
         print_args = f"args={str(self.args)}" if self.log_args else ""
         print_kwargs = ""
-        
+
         if self.log_kwargs == "all":
             print_kwargs = f"kwargs={str(self.kwargs)}"
         elif self.log_kwargs:
-            print_kwargs = f"{self.log_kwargs}={self.kwargs.get(self.log_kwargs)}"
-        
+            print_kwargs = (
+                f"{self.log_kwargs}={self.kwargs.get(self.log_kwargs)}"
+            )
+
         return print_args, print_kwargs
 
+    def _get_module_name(self) -> str:
+        module = self.function.__module__
+        if module is None or module == str.__class__.__module__:
+            return ""
+        if module == "__main__":
+            return "main"
+        return module
+
     def _get_function_name(self) -> str:
-        cls = self.function.__self__.__class__ if hasattr(self.function, '__self__') else None
-        return f"{cls.__name__}.{self.function.__name__}" if cls else self.function.__name__
+        cls = (
+            self.function.__self__.__class__
+            if hasattr(self.function, "__self__")
+            else None
+        )
+        return (
+            f"{cls.__name__}.{self.function.__name__}"
+            if cls
+            else self.function.__name__
+        )
 
-    def _log_message(self, action: str, run_time: Optional[float] = None) -> None:
-        if not self.logger:
-            return
-
+    def _get_full_function_name(self) -> str:
         func_name = self._get_function_name()
+        if self.module_name:
+            return f"{self.module_name}.{func_name}"
+        return func_name
+
+    def _log(self, message: str) -> None:
+        if self.logger:
+            self.logger.info(message)
+        else:
+            print(message)
+
+    def _log_message(
+        self, action: str, run_time: Optional[float] = None
+    ) -> None:
+        func_name = self._get_full_function_name()
         args, kwargs = self._format_arguments()
-        
+
         message = f"{action} {func_name}()"
         if run_time is not None:
             message += f" in {run_time:.4f} secs"
@@ -63,8 +85,7 @@ class Profiler:
         if kwargs:
             message += f", {kwargs}"
 
-        print(message)
-        self.logger.info(message)
+        self._log(message)
 
     def start(self) -> None:
         self.start_time = time.perf_counter()
@@ -76,15 +97,14 @@ class Profiler:
         self._log_message("Finished", run_time)
 
 
-
 def profiling(
-    logger: Logger,
+    logger: Optional[Logger] = None,
     log_start: bool = False,
     log_args: bool = False,
-    log_kwargs: str = None,
+    log_kwargs: Optional[str] = None,
 ):
     """
-    We will write all the result into logger
+    We will write all the result into logger if provided, otherwise use print
     log_args can be true or false. If it is true, it will print all the args passed to the function in the log.
     log_kwargs can be None, "all" or argument_name to be logged.
     """
@@ -100,6 +120,7 @@ def profiling(
                 log_args=log_args,
                 log_kwargs=log_kwargs,
             )
+
         @wraps(f)
         def wrapper(*args, **kwargs):
             profiler = create_profiler(*args, **kwargs)
--- time-logger/test.py
+++ time-logger/test.py
@@ -5,25 +5,26 @@ from profiling import profiling
 logger = logging.getLogger()
 logger.addHandler(logging.StreamHandler())
 
-
-@profiling(logger=logger, log_start=True)
+@profiling(log_start=True)
 def run_400(time=200):
     for i in range(time):
         i *= i
 
 
-@profiling(logger=logger, log_start=False)
+@profiling(logger=None, log_start=False)
 def run_500(time=200):
     for i in range(time):
         i *= i
 
+
 @profiling(logger=logger, log_args=True, log_start=True)
 def sum(*args):
-    sum =0
+    sum = 0
     for i in args:
         sum += i
     return sum
 
+
 run_400(20)
 run_500(20)
-sum(1,2,3,4)
+sum(1, 2, 3, 4)
